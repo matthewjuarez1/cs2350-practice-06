f2a2d934735b533e54ed5c84c1c9f926fbc72a71
Matthew Juarez <matthewjuarez1@mail.weber.edu>
2020-03-14 13:52:09 -0600

    Hello

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -4,11 +4,11 @@ require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
 require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
-//TODO
+//TODO3
 
-var name = document.getElementById("name-fld")
+
 function displayMessage(){
-
+  var name = document.getElementById("name-fld").value
   alert("WELCOME " + name + "!")
 
 }
